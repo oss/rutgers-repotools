08f0853d28c71067b567338fe88717e2b923538f
Kyle Suarez <ksuarz@gmail.com>
2014-01-30 17:15:35 -0500

    Should not use argparse in setup

--- setup.py
+++ setup.py
@@ -13,44 +13,30 @@ for arg in range(len(args)):
 if "install" in args:
     dir_util.mkpath(buildroot + '/var/log/rutgers-repotools')
 
-if __name__ == '__main__':
-    # Set up command line options
-    parser = argparse.ArgumentParser(
-            prog='setup',
-            description='setup - installs rutgers-repotools',
-            epilog='')
-    parser.add_argument(
-            '--buildroot',
-            help='The buildroot to install to.')
-
-    # Parse the command line arguments
-    namespace = parser.parse_args()
-    buildroot = namespace.buildroot
-
-    # Finally, run the setup
-    setup(name         = 'rutgers-repotools',
-          version      = '0.7.0',
-          description  = 'Dependency checking and publish scripts',
-          author       = 'Open System Solutions',
-          author_email = 'oss@oss.rutgers.edu',
-          url          = 'https://github.com/oss/rutgers-repotools/',
-          license      = 'GPLv2+',
-          platforms    = ['linux'],
-          long_description  = """This package contains Rutgers tools needed to check
-            the dependencies of new packages. This also installs a daily cron job
-            which checks for broken dependencies in the Rutgers tree and sends email
-            to report problems.
-          """,
-          packages     = ['RUtools'],
-          package_dir  = {'RUtools': 'lib'},
-          data_files   = [('/etc', ['conf/depcheck.ignore.sample', 'conf/rutgers-repotools.cfg.sample']),
-                          ('/etc/cron.daily', ['cron/daily_checks'])],
-          scripts      = ['bin/depcheck',
-                          'bin/koji-backup',
-                          'bin/movepackage',
-                          'bin/populate-rpmfind-db',
-                          'bin/pullpackage',
-                          'bin/pushpackage',
-                          'bin/rebuild-repos',
-                          'bin/repocheck',
-                          'bin/rpmdb-backup'])
+# Finally, run the setup
+setup(name         = 'rutgers-repotools',
+      version      = '0.7.0',
+      description  = 'Dependency checking and publish scripts',
+      author       = 'Open System Solutions',
+      author_email = 'oss@oss.rutgers.edu',
+      url          = 'https://github.com/oss/rutgers-repotools/',
+      license      = 'GPLv2+',
+      platforms    = ['linux'],
+      long_description  = """This package contains Rutgers tools needed to check
+        the dependencies of new packages. This also installs a daily cron job
+        which checks for broken dependencies in the Rutgers tree and sends email
+        to report problems.
+      """,
+      packages     = ['RUtools'],
+      package_dir  = {'RUtools': 'lib'},
+      data_files   = [('/etc', ['conf/depcheck.ignore.sample', 'conf/rutgers-repotools.cfg.sample']),
+                      ('/etc/cron.daily', ['cron/daily_checks'])],
+      scripts      = ['bin/depcheck',
+                      'bin/koji-backup',
+                      'bin/movepackage',
+                      'bin/populate-rpmfind-db',
+                      'bin/pullpackage',
+                      'bin/pushpackage',
+                      'bin/rebuild-repos',
+                      'bin/repocheck',
+                      'bin/rpmdb-backup'])
