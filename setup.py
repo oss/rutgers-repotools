b3970efcd462c6ace14c26731041ac0225575e6b
Kyle Suarez <ksuarz@gmail.com>
2014-10-17 12:49:36 -0400

    Update to version 0.7.13

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if "install" in args:
 
 # Finally, run the setup
 setup(name         = 'rutgers-repotools',
-      version      = '0.7.12',
+      version      = '0.7.13',
       description  = 'Dependency checking and publish scripts',
       author       = 'Open System Solutions',
       author_email = 'oss@oss.rutgers.edu',
